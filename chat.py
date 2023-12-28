0cad363952a2b43ec931b40158a54217963774bb
KOUSTAVSAMANTA <67233274+KOUSTAVSAMANTA@users.noreply.github.com>
2023-12-28 21:25:41 +0530

    Update chat.py

--- chat.py
+++ chat.py
@@ -1,9 +1,15 @@
 # Author - MrSentinel
-
+from langchain_google_genai import ChatGoogleGenerativeAI
 import streamlit as st
 import google.generativeai as genai
 import google.ai.generativelanguage as glm
-# from dotenv import load_dotenv
+from langchain.agents import Tool, AgentExecutor, LLMSingleActionAgent, AgentOutputParser
+from langchain.prompts import StringPromptTemplate
+from langchain.utilities import SerpAPIWrapper
+from langchain.chains import LLMChain
+from typing import List, Union
+from langchain.schema import AgentAction, AgentFinish, OutputParserException
+import re
 from PIL import Image
 import os
 import io
@@ -11,7 +17,104 @@ import io
 # load_dotenv()
 
 os.environ["GOOGLE_API_KEY"] ="AIzaSyDqKzb2p4ItiEEao-oim5IcGgAifOtv6do"
+os.environ["SERPAPI_API_KEY"] ="f6384dec8fb9a930c7c2c1e39145d282f28ab8f1500d129aa98304fe7f394c33"
 
+
+search = SerpAPIWrapper()
+tools = [
+    Tool(
+        name="Search",
+        func=search.run,
+        description="useful for when you need to answer questions about current events"
+    )
+]
+# Set up the base template
+template = """Answer the following questions as best you can. You have access to the following tools:
+
+{tools}
+
+Use the following format:
+
+Question: the input question you must answer
+Thought: you should always think about what to do
+Action: the action to take, should be one of [{tool_names}]
+Action Input: the input to the action
+Observation: the result of the action
+... (this Thought/Action/Action Input/Observation can repeat 2-3 times)
+Thought: I now know the final answer
+Final Answer: the final answer to the original input question
+
+Begin!
+
+Question: {input}
+{agent_scratchpad}"""
+
+# Set up a prompt template
+class CustomPromptTemplate(StringPromptTemplate):
+    # The template to use
+    template: str
+    # The list of tools available
+    tools: List[Tool]
+
+    def format(self, **kwargs) -> str:
+        # Get the intermediate steps (AgentAction, Observation tuples)
+        # Format them in a particular way
+        intermediate_steps = kwargs.pop("intermediate_steps")
+        thoughts = ""
+        for action, observation in intermediate_steps:
+            thoughts += action.log
+            thoughts += f"\nObservation: {observation}\nThought: "
+        # Set the agent_scratchpad variable to that value
+        kwargs["agent_scratchpad"] = thoughts
+        # Create a tools variable from the list of tools provided
+        kwargs["tools"] = "\n".join([f"{tool.name}: {tool.description}" for tool in self.tools])
+        # Create a list of tool names for the tools provided
+        kwargs["tool_names"] = ", ".join([tool.name for tool in self.tools])
+        return self.template.format(**kwargs)
+
+prompt = CustomPromptTemplate(
+    template=template,
+    tools=tools,
+    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+    # This includes the `intermediate_steps` variable because that is needed
+    input_variables=["input", "intermediate_steps"]
+)
+
+class CustomOutputParser(AgentOutputParser):
+
+    def parse(self, llm_output: str) -> Union[AgentAction, AgentFinish]:
+        # Check if agent should finish
+        if "Final Answer:" in llm_output:
+            return AgentFinish(
+                # Return values is generally always a dictionary with a single `output` key
+                # It is not recommended to try anything else at the moment :)
+                return_values={"output": llm_output.split("Final Answer:")[-1].strip()},
+                log=llm_output,
+            )
+        # Parse out the action and action input
+        regex = r"Action\s*\d*\s*:(.*?)\nAction\s*\d*\s*Input\s*\d*\s*:[\s]*(.*)"
+        match = re.search(regex, llm_output, re.DOTALL)
+        if not match:
+            raise OutputParserException(f"Could not parse LLM output: `{llm_output}`")
+        action = match.group(1).strip()
+        action_input = match.group(2)
+        # Return the action and action input
+        return AgentAction(tool=action, tool_input=action_input.strip(" ").strip('"'), log=llm_output)
+
+def get_res(q):
+    output_parser = CustomOutputParser()
+    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    llm_chain = LLMChain(llm=llm, prompt=prompt)
+    tool_names = [tool.name for tool in tools]
+    agent = LLMSingleActionAgent(
+        llm_chain=llm_chain,
+        output_parser=output_parser,
+        stop=["Observation"],
+        allowed_tools=tool_names
+    )
+    agent_executor = AgentExecutor.from_agent_and_tools(agent=agent, tools=tools, verbose=True)
+    z = agent_executor.run(q)
+    return z
 def image_to_byte_array(image: Image) -> bytes:
     imgByteArr = io.BytesIO()
     image.save(imgByteArr, format=image.format)
@@ -25,25 +128,25 @@ genai.configure(api_key=API_KEY)
 # st.image("./Google-Gemini-AI-Logo.png", width=200)
 st.write("")
 
-gemini,gemini_vision = st.tabs(["CODA","CODA Vision"])
+gemini_pro, gemini_vision = st.tabs(["CODA Web", "CODA Vision"])
 
 
 def main():
-    # with gemini_pro:
-    #     st.header("Interact with CODA")
-    #     st.write("")
+    with gemini_pro:
+        st.header("Interact with CODA")
+        st.write("")
 
-    #     prompt = st.text_input("prompt please...", placeholder="Prompt", label_visibility="visible")
-    #     model = genai.GenerativeModel("gemini-pro")
+        prompt = st.text_input("prompt please...", placeholder="Prompt", label_visibility="visible")
+        # model = genai.GenerativeModel("gemini-pro")
 
-    #     if st.button("SEND", use_container_width=True):
-    #         response = model.generate_content(prompt)
+        if st.button("SEND", use_container_width=True):
+            response = get_res(prompt)
 
-    #         st.write("")
-    #         st.header(":blue[Response]")
-    #         st.write("")
+            st.write("")
+            st.header(":blue[Response]")
+            st.write("")
 
-    #         st.markdown(response.text)
+            st.markdown(response)
 
     with gemini_vision:
         st.header("Interact with CODA Vision")
